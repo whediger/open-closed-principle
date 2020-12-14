fff650d4a62eb952b3bade4104176d02e9ee4df0
HED2084 <wes.hediger@gmail.com>
2020-12-13 19:38:19 -0500

    initial commit

---
+++ exorcises/MusicPlayer.java
@@ -0,0 +1,20 @@
+
+public class MusicPlayer {
+
+	public static void main(String args[]) {
+		MusicPlayer musicPlayer = new MusicPlayer();
+		musicPlayer.playMusic("streamingApp");
+		musicPlayer.playMusic("record player");
+	}
+
+	private void playMusic(String playerType) {
+		if(playerType == "streamingApp"){
+			System.out.println("favorite playlist now streaming");
+		} else if(playerType == "recordPlayer"){
+			System.out.println("spinning record and music is playing");
+		} else {
+			System.out.println("music player not supported, plese choose another");
+		}
+	}
+
+}
